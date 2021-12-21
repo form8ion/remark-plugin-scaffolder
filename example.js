5460cc9a1be88ac6eb2aa8eb89592ed2be32df98
Matt Travi <programmer@travi.org>
2021-12-20 23:38:09 -0600

    docs(example): provided node_modules to the stubbed file system

--- example.js
+++ example.js
@@ -1,10 +1,14 @@
 // #### Import
-// remark-usage-ignore-next
+// remark-usage-ignore-next 2
+import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import {scaffold} from './lib/index.cjs';
 
-// remark-usage-ignore-next
-stubbedFs();
+// remark-usage-ignore-next 4
+stubbedFs({
+  node_modules: stubbedFs.load(resolve(__dirname, 'node_modules')),
+  templates: stubbedFs.load(resolve(__dirname, 'templates'))
+});
 
 // #### Execute
 
@@ -14,6 +18,3 @@ scaffold({
   packageName: '@scope/name-of-the-package',
   tests: {integration: true}
 });
-
-// remark-usage-ignore-next
-stubbedFs.restore();
